df39867ee32eb3fe6b4bd6bdd95d4c711e5f94d8
moohh91 <moohh91@gmail.com>
2015-03-21 17:13:50 -0400

    Revert "Added student class."
    This reverts commit 31aead45fa088f89f17bcd257db70df36b694619.

--- CApp/CApp/Program.cs
+++ CApp/CApp/Program.cs
@@ -10,11 +10,6 @@ namespace CApp
     {
         static void Main(string[] args)
         {
-            var student = new Student
-            {
-                FirstName = "Mohammed",
-                LastName = "Hussain"
-            };
         }
     }
 }
