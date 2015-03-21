0340780203b5218917d24d6fd17efa699d403531
moohh91 <moohh91@gmail.com>
2015-03-21 16:43:26 -0400

    Expression bodies members.

---
+++ CApp/CApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = new Student
+            {
+                FirstName = "Mohammed",
+                LastName = "Hussain"
+            };
+
+            Console.WriteLine(student.FullName);
+        }
+    }
+}
---
+++ CApp/CApp/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CApp
+{
+    public class Student
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String FullName
+        {
+            get
+            {
+                return String.Format("{0} {1}", FirstName, LastName);
+            }
+        }
+    }
+}
